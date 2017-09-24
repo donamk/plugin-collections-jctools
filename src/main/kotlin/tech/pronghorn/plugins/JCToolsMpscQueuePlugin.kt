e2704b5be1ef02b3598894d20fc062d43eb32d60
Chris Turner <chris@pronghorn.tech>
2017-09-24 00:48:47 -0600

    Added support for unbounded queues.

--- src/main/kotlin/tech/pronghorn/plugins/JCToolsMpscQueuePlugin.kt
+++ src/main/kotlin/tech/pronghorn/plugins/JCToolsMpscQueuePlugin.kt
@@ -17,9 +17,12 @@
 package tech.pronghorn.plugins
 
 import org.jctools.queues.MpscArrayQueue
+import org.jctools.queues.MpscUnboundedArrayQueue
 import tech.pronghorn.plugins.mpscQueue.MpscQueuePlugin
 import java.util.Queue
 
 object JCToolsMpscQueuePlugin : MpscQueuePlugin {
-    override fun <T> get(capacity: Int): Queue<T> = MpscArrayQueue(capacity)
+    override fun <T> getBounded(capacity: Int): Queue<T> = MpscArrayQueue(capacity)
+
+    override fun <T> getUnbounded(): Queue<T> = MpscUnboundedArrayQueue(1024)
 }
--- src/main/kotlin/tech/pronghorn/plugins/JCToolsSpscQueuePlugin.kt
+++ src/main/kotlin/tech/pronghorn/plugins/JCToolsSpscQueuePlugin.kt
@@ -17,9 +17,12 @@
 package tech.pronghorn.plugins
 
 import org.jctools.queues.SpscArrayQueue
+import org.jctools.queues.SpscUnboundedArrayQueue
 import tech.pronghorn.plugins.spscQueue.SpscQueuePlugin
 import java.util.Queue
 
 object JCToolsSpscQueuePlugin : SpscQueuePlugin {
-    override fun <T> get(capacity: Int): Queue<T> = SpscArrayQueue(capacity)
+    override fun <T> getBounded(capacity: Int): Queue<T> = SpscArrayQueue(capacity)
+
+    override fun <T> getUnbounded(): Queue<T> = SpscUnboundedArrayQueue(1024)
 }
